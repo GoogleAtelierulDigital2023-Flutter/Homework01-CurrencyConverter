86b9a42ac641ebf6d38b4977289504b900bc878c
Iulian POPA <iulian.popa@zitec.com>
2023-04-10 16:29:54 +0300

    Add Homework 1

---
+++ android/app/src/main/kotlin/com/example/gad_2023_flutter_homework_01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gad_2023_flutter_homework_01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Currency Converter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Currency convertor'),
+          ),
+          body: const CurrencyConvertor(),
+        ),
+      ),
+    );
+  }
+}
+
+class CurrencyConvertor extends StatefulWidget {
+  const CurrencyConvertor({super.key});
+
+  @override
+  State<CurrencyConvertor> createState() => CurrencyConvertorState();
+}
+
+class CurrencyConvertorState extends State<CurrencyConvertor> {
+  final _textController = TextEditingController();
+
+  static const _exchangeRate = 4.9;
+
+  bool _isError = false;
+  double _exchangeResult = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Image.asset('assets/images/bani.jpg'),
+          TextField(
+            controller: _textController,
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(12.0),
+              errorText: _isError ? 'Please enter a valid amount' : null,
+              labelText: 'Enter the amount in EUR',
+            ),
+            inputFormatters: <TextInputFormatter>[
+              FilteringTextInputFormatter.allow(RegExp(r'[0-9]|\.')),
+            ],
+            keyboardType: const TextInputType.numberWithOptions(decimal: true),
+            onChanged: (value) {
+              setState(
+                () {
+                  _isError = false;
+                },
+              );
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              final inputNumber = double.tryParse(_textController.text);
+
+              setState(() {
+                if (inputNumber == null || inputNumber == 0) {
+                  _isError = true;
+                } else {
+                  _exchangeResult = inputNumber * _exchangeRate;
+                }
+              });
+            },
+            child: const Text('CONVERT'),
+          ),
+          Text(
+            _exchangeResult == 0 ? '' : '${_exchangeResult.toStringAsFixed(2)} RON',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ],
+      ),
+    );
+  }
+}
